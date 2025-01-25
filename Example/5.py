c0cbc9fbc6063203778bbb8565c0c2ded841a485
omsuneri <omsuneri@gmail.com>
2025-01-25 18:55:07 +0530

    Adding Examples

---
+++ Example/5.py
@@ -0,0 +1,18 @@
+# Adding two numbers 
+a = 5
+b = 6
+print(a + b)
+
+# Type of a variable
+print(type(a))  
+print(type(b))
+
+# Typecasting of a variable
+d = int(input("Enter a number: "))
+print(type(d))
+
+# comparing two numbers
+print(a > b)        
+
+# n power of a number
+print(5**3)
